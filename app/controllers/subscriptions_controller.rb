fd69a400bb00f1ef88bf84a40a99696dffbcf084
James Somers <jsomers@gmail.com>
2012-03-07 00:46:12 -0500

    Refactor subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,6 +2,7 @@ class SubscriptionsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @subscriptions = current_user.subscriptions.joins("JOIN feeds ON subscriptions.feed_id = feeds.id").order("feeds.last_modified DESC")
   end
   
   def create
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -14,25 +14,28 @@ class Feed < ActiveRecord::Base
         has_many :outlines, Outline
     end
   end
-  
+
   def self.seed(feed_url)
-    feed = Feedzirra::Feed.fetch_and_parse(feed_url)
-    if !feed.is_a?(Fixnum) && feed.present?
-      find_or_create_by_url(feed.url, feed_url: feed.feed_url, title: feed.title.try(:strip), last_modified: feed.last_modified)
-    else
-      find_or_create_by_feed_url(feed_url)
-    end
+    make_if_necessary(Feedzirra::Feed.fetch_and_parse(feed_url), feed_url)
   end
   
   def self.all_for_opml(opml)
     feed_urls = OPML::Outline.parse(opml).map(&:xmlUrl).reject {|feed_url| find_by_feed_url(feed_url).present?}
     Feedzirra::Feed.fetch_and_parse(feed_urls).inject([]) do |feeds, feed|
       feed_url, feed = feed
-      if !feed.is_a?(Fixnum) && feed.present?
-        feeds << find_or_create_by_url(feed.url, feed_url: feed.feed_url, title: feed.title.try(:strip), last_modified: feed.last_modified)
-      else
-        feeds << find_or_create_by_feed_url(feed_url)
-      end
+      feeds << make_if_necessary(feed, feed_url)
+    end
+  end
+  
+  def self.make_if_necessary(feed, feed_url)
+    if !feed.is_a?(Fixnum) && feed.present?
+      find_or_create_by_url(feed.url, 
+        feed_url: feed.feed_url,
+        title: feed.title.try(:strip), 
+        last_modified: feed.last_modified
+      )
+    else
+      find_or_create_by_feed_url(feed_url)
     end
   end
   
