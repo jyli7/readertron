9eb6648555b6faf447f08314fe4795176f2cdd66
James Somers <jsomers@gmail.com>
2012-03-09 19:01:53 -0500

    Subscription li cleanup

--- app/helpers/reader_helper.rb
+++ app/helpers/reader_helper.rb
@@ -6,4 +6,15 @@ module ReaderHelper
       date.strftime("%b #{date.day}, %Y %I:%M %p")
     end
   end
+  
+  def li_classes(subscription, feed_id)
+    classes = []
+    classes << "selected" if subscription.feed.id == feed_id.to_i
+    classes << "unread" if subscription.unread_count > 0
+    classes.join(" ")
+  end
+  
+  def unread_count(subscription)
+     subscription.unread_count > 0 ? "(#{subscription.unread_count})" : ""
+  end
 end
