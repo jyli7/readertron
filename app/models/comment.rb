5ef71f0c021dbaea30b93fb7aa382a62963e9282
James Somers <jsomers@gmail.com>
2012-03-21 03:09:35 -0400

    Don't send an email when you yourself are the one commenting.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,8 +8,8 @@ class Comment < ActiveRecord::Base
   after_create :notify_relevant_users
   
   def notify_relevant_users
-    relevant_users = [post.sharer] | (post.comments.map(&:user) - [user])
-    (relevant_users).each do |u|
+    relevant_users = [post.sharer] | post.comments.map(&:user)
+    (relevant_users - [user]).each do |u|
       ShareMailer.new_comment_email(u, self).deliver
     end
   end
