7cd6af661a5b56214026a8195620a1866529b9c3
James Somers <jsomers@gmail.com>
2012-03-22 02:00:44 -0400

    Use Rackspace mail server.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,17 +53,16 @@ Readertron::Application.configure do
   
   # Mailer
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'readertron.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'baci.lindsaar.net',
-    :user_name            => 'jsomers',
-    :password             => 'uamxxnbgtsefttts',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+  config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_url_options = { :host => 'readertron.com' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address              => "secure.emailsrvr.com",
+      :port                 => 587,
+      :user_name            => 'notifications@readertron.com',
+      :password             => 'H0fst4dter!',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
   
 
   # Enable threaded mode
