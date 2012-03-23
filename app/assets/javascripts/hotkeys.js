c85d7122784439d46357f75c9c341510d361ae52
James Somers <jsomers@gmail.com>
2012-03-23 03:17:14 -0400

    Simple navigational / commenting keyboard shortcuts in "My Shared Items" view.

--- app/assets/javascripts/hotkeys.js
+++ app/assets/javascripts/hotkeys.js
@@ -1,25 +1,13 @@
 var g_mark = false;
 
 $(document).ready(function() {
-	if ($("#panes").length > 0) {
+	if ($("#right-pane").length > 0) {
 		$(document).bind('keydown', 'j', function(e) {
 			next_post(+1); return false;
 		});
 
 		$(document).bind('keydown', 'k', function(e) {
 			next_post(-1); return false;
-		});
-
-		$(document).bind('keydown', 'm', function(e) {
-			$(".entry.current").toggle("span.read-state", "read-state-kept-unread", mark_as_read); return false;
-		});
-
-		$(document).bind('keydown', 's', function(e) {
-			$(".entry.current").toggle("span.item-star", "star-inactive", set_shared_status); return false;
-		});
-
-		$(document).bind('keydown', 'n', function(e) {
-			$(".entry.current").toggle("span.share-with-note", "share-with-note-inactive", set_shared_with_note_status); return false;
 		});
 		
 		$(document).bind('keydown', 'c', function(e) {
@@ -30,6 +18,20 @@ $(document).ready(function() {
 			if ($(".entry.current").length > 0) {
 				window.open($(".entry.current").find(".entry-title-link").attr("href"), '_blank');
 			};
+		});
+	};
+	
+	if ($("#panes").length > 0) {
+		$(document).bind('keydown', 'm', function(e) {
+			$(".entry.current").toggle("span.read-state", "read-state-kept-unread", mark_as_read); return false;
+		});
+
+		$(document).bind('keydown', 's', function(e) {
+			$(".entry.current").toggle("span.item-star", "star-inactive", set_shared_status); return false;
+		});
+
+		$(document).bind('keydown', 'n', function(e) {
+			$(".entry.current").toggle("span.share-with-note", "share-with-note-inactive", set_shared_with_note_status); return false;
 		});
 		
 		$(document).bind('keydown', 'g', function(e) {
