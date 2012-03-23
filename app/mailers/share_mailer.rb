3dec89278e8dbfb835648eac83d0223fb1f0f9e0
James Somers <jsomers@gmail.com>
2012-03-23 03:13:13 -0400

    Mail should come from the name "Readertron", not just "notifications"

--- app/mailers/share_mailer.rb
+++ app/mailers/share_mailer.rb
@@ -1,6 +1,6 @@
 class ShareMailer < ActionMailer::Base
   add_template_helper(ApplicationHelper)
-  default from: "notifications@readertron.com"
+  default from: "Readertron <notifications@readertron.com>"
   
   def new_comment_email(user, comment)
     @user = user
